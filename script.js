25f68b223ec33e6731637adb58b290bb1e74871c
toniamoser <tonia.moser@stud.fhgr.ch>
2024-06-19 15:01:17 +0200

    fixed search field

--- script.js
+++ script.js
@@ -40,49 +40,51 @@ async function init() {
   initMap();
 }
 
-async function initMap(lat = 8.536919584914061, long = 47.38059039878863) {
+async function initMap(lat = 8.536919584914061, long = 47.38059039878863, redrawInput = true) {
   let ScooterDaten = await holeScooterDaten(`https://api.sharedmobility.ch/v1/sharedmobility/identify?filters=ch.bfe.sharedmobility.vehicle_type=E-Scooter&Geometry=${lat},${long}&Tolerance=1000&offset=0&gemetryFormat=esrijson`);
   console.log(ScooterDaten);
   let mapBoxArray = datenInArray(ScooterDaten);
-  drawMap(mapBoxArray, lat, long)
+  drawMap(mapBoxArray, lat, long, redrawInput)
+
+  console.log(redrawInput);
 }
 initMap();
 
 let geocoder; 
 
-function drawMap(pointArray, lat = 8.538, long = 47.38) {
+function drawMap(pointArray, lat = 8.538, long = 47.38, redrawInput = true) {
   mapboxgl.accessToken = 'pk.eyJ1IjoibHVrYXNzY2hsZWdlbCIsImEiOiJjbHc2Y2J3YngxcXRiMmxweWIwM3V3eWg0In0.grSxvL6hdG7c-8UeuDq2rA';
   const map = new mapboxgl.Map({
-  container: 'map',
-  countries: 'ch',
-  style: 'mapbox://styles/mapbox/light-v11',
-  center: [lat, long],
-  zoom: 14.2,
+    container: 'map',
+    countries: 'ch',
+    style: 'mapbox://styles/mapbox/light-v11',
+    center: [lat, long],
+    zoom: 14.2,
   });
   
   const geojson = {
-      type: 'FeatureCollection',
-      features: pointArray
+    type: 'FeatureCollection',
+    features: pointArray
   };
 
   for (const feature of geojson.features) {
-  const el = document.createElement('div');
-  el.className = 'marker';
+    const el = document.createElement('div');
+    el.className = 'marker';
 
-  new mapboxgl.Marker(el).setLngLat(feature.geometry.coordinates).addTo(map); 
+    new mapboxgl.Marker(el).setLngLat(feature.geometry.coordinates).addTo(map); 
 
-  new mapboxgl.Marker(el)
-    .setLngLat(feature.geometry.coordinates)
-    .setPopup(
-      new mapboxgl.Popup({ offset: 25 })
-        .setHTML(
-          `<p class="describtion">${feature.properties.description}</p>`
-        )
-    )
-    .addTo(map);
+    new mapboxgl.Marker(el)
+      .setLngLat(feature.geometry.coordinates)
+      .setPopup(
+        new mapboxgl.Popup({ offset: 25 })
+          .setHTML(
+            `<p class="describtion">${feature.properties.description}</p>`
+          )
+      )
+      .addTo(map);
   };
   
-   map.addControl(
+  map.addControl(
     new mapboxgl.GeolocateControl({
         positionOptions: {
             enableHighAccuracy: true
@@ -90,33 +92,34 @@ function drawMap(pointArray, lat = 8.538, long = 47.38) {
         trackUserLocation: true,
         showUserHeading: true
     })
-);
+  );
 
-const geocoder = new MapboxGeocoder({
-  accessToken: 'pk.eyJ1IjoibHVrYXNzY2hsZWdlbCIsImEiOiJjbHc2Y2J3YngxcXRiMmxweWIwM3V3eWg0In0.grSxvL6hdG7c-8UeuDq2rA',
-  mapboxgl: 0,
-  countries: 'CH',
-  lanugage: 'de',
-  bbox: [6.02260949059, 45.7769477403, 10.4427014502, 47.8308275417],
-  placeholder: 'Suche nach einer Adresse',
-  marker: false,
-  feature_type: 'place' + 'address',
-        render: function (item) {
-            const maki = item.properties.maki || 'marker';
-            return `<div class='geocoder-dropdown-item'>
-                    <img class='geocoder-dropdown-icon' src='https://unpkg.com/@mapbox/maki@6.1.0/icons/${maki}-15.svg'>
-                    <span class='geocoder-dropdown-text'>
-                    ${item.text} </span>
-                    </div>`;
-        },
-
-})
-
+  const geocoder = new MapboxGeocoder({
+    accessToken: 'pk.eyJ1IjoibHVrYXNzY2hsZWdlbCIsImEiOiJjbHc2Y2J3YngxcXRiMmxweWIwM3V3eWg0In0.grSxvL6hdG7c-8UeuDq2rA',
+    mapboxgl: 0,
+    countries: 'CH',
+    lanugage: 'de',
+    bbox: [6.02260949059, 45.7769477403, 10.4427014502, 47.8308275417],
+    placeholder: 'Suche nach einer Adresse',
+    marker: false,
+    feature_type: 'place' + 'address',
+          render: function (item) {
+              const maki = item.properties.maki || 'marker';
+              return `<div class='geocoder-dropdown-item'>
+                      <img class='geocoder-dropdown-icon' src='https://unpkg.com/@mapbox/maki@6.1.0/icons/${maki}-15.svg'>
+                      <span class='geocoder-dropdown-text'>
+                      ${item.text} </span>
+                      </div>`;
+          },
+  })
+  
   geocoder.on('result', (selected) => {
-    initMap(selected.result.center[0], selected.result.center[1]);
+    initMap(selected.result.center[0], selected.result.center[1], false);
   }) 
   
-  document.getElementById('geocoder').appendChild(geocoder.onAdd(map));
+  if (redrawInput) {
+    document.getElementById('geocoder').appendChild(geocoder.onAdd(map));
+  }
 }
 
 
